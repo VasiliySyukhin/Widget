2b20a93b58b4c00b38a978ea0c6cc94beafcc82d
VasiliySyukhin <vasiliysyukhin@gmail.com>
2022-06-13 18:38:05 +0300

    Widget

---
+++ Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace WidgetWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_Usd = new System.Windows.Forms.Label();
+            this.lbl_Eur = new System.Windows.Forms.Label();
+            this.Bts = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.NewDocument = new WidgetWindows.DesignUI();
+            this.Open = new WidgetWindows.DesignUI();
+            this.Save = new WidgetWindows.DesignUI();
+            this.Close = new WidgetWindows.DesignUI();
+            this.Time = new System.Windows.Forms.Label();
+            this.Date = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ebrima", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ebrima", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "EUR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ebrima", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "BTS";
+            // 
+            // lbl_Usd
+            // 
+            this.lbl_Usd.AutoSize = true;
+            this.lbl_Usd.Font = new System.Drawing.Font("Ebrima", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Usd.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_Usd.Location = new System.Drawing.Point(68, 19);
+            this.lbl_Usd.Name = "lbl_Usd";
+            this.lbl_Usd.Size = new System.Drawing.Size(56, 32);
+            this.lbl_Usd.TabIndex = 3;
+            this.lbl_Usd.Text = "123";
+            // 
+            // lbl_Eur
+            // 
+            this.lbl_Eur.AutoSize = true;
+            this.lbl_Eur.Font = new System.Drawing.Font("Ebrima", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Eur.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_Eur.Location = new System.Drawing.Point(68, 51);
+            this.lbl_Eur.Name = "lbl_Eur";
+            this.lbl_Eur.Size = new System.Drawing.Size(56, 32);
+            this.lbl_Eur.TabIndex = 4;
+            this.lbl_Eur.Text = "123";
+            // 
+            // Bts
+            // 
+            this.Bts.AutoSize = true;
+            this.Bts.Font = new System.Drawing.Font("Ebrima", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Bts.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Bts.Location = new System.Drawing.Point(68, 83);
+            this.Bts.Name = "Bts";
+            this.Bts.Size = new System.Drawing.Size(56, 32);
+            this.Bts.TabIndex = 5;
+            this.Bts.Text = "123";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 136);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(112, 132);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 310);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 99);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.OnTextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(155, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 21);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "123";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(128, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 21);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Архангельск";
+            // 
+            // NewDocument
+            // 
+            this.NewDocument.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.NewDocument.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NewDocument.ForeColor = System.Drawing.Color.White;
+            this.NewDocument.Location = new System.Drawing.Point(12, 276);
+            this.NewDocument.Name = "NewDocument";
+            this.NewDocument.RoundingEnable = true;
+            this.NewDocument.Size = new System.Drawing.Size(112, 23);
+            this.NewDocument.TabIndex = 14;
+            this.NewDocument.Text = "Создать заметку";
+            this.NewDocument.Click += new System.EventHandler(this.NewDocument_Click);
+            // 
+            // Open
+            // 
+            this.Open.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.Open.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Open.ForeColor = System.Drawing.Color.White;
+            this.Open.Location = new System.Drawing.Point(14, 418);
+            this.Open.Name = "Open";
+            this.Open.RoundingEnable = true;
+            this.Open.Size = new System.Drawing.Size(78, 23);
+            this.Open.TabIndex = 15;
+            this.Open.Text = "Открыть";
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Save
+            // 
+            this.Save.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.Save.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Save.ForeColor = System.Drawing.Color.White;
+            this.Save.Location = new System.Drawing.Point(99, 418);
+            this.Save.Name = "Save";
+            this.Save.RoundingEnable = true;
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 16;
+            this.Save.Text = "Сохранить";
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Close
+            // 
+            this.Close.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.Close.ForeColor = System.Drawing.Color.White;
+            this.Close.Location = new System.Drawing.Point(224, 0);
+            this.Close.Name = "Close";
+            this.Close.RoundingEnable = false;
+            this.Close.Size = new System.Drawing.Size(23, 22);
+            this.Close.TabIndex = 17;
+            this.Close.Text = "X";
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Time.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Time.Location = new System.Drawing.Point(150, 154);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(48, 21);
+            this.Time.TabIndex = 18;
+            this.Time.Text = "Time";
+            // 
+            // Date
+            // 
+            this.Date.AutoSize = true;
+            this.Date.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Date.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Date.Location = new System.Drawing.Point(142, 199);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(46, 21);
+            this.Date.TabIndex = 19;
+            this.Date.Text = "Date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(153, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 21);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Время";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(159, 176);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 21);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Дата";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.ClientSize = new System.Drawing.Size(247, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.NewDocument);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Bts);
+            this.Controls.Add(this.lbl_Eur);
+            this.Controls.Add(this.lbl_Usd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label lbl_Usd;
+        private Label lbl_Eur;
+        private Label Bts;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private TextBox textBox1;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private Label label4;
+        private Label label5;
+        private DesignUI NewDocument;
+        private DesignUI Open;
+        private DesignUI Save;
+        private DesignUI Close;
+        private Label Time;
+        private Label Date;
+        private Label label6;
+        private Label label7;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
